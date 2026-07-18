2c1097a3e7cdb08f48267304e57f64b68a280803
thagreatjoel <joeljonah06@gmail.com>
2026-07-18 11:12:09 +0530

    cc

--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
+
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'github.com',
+      },
+    ],
+  },
  
 };
 

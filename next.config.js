9f2c4bc74345f0d0d6885e370a79cd6532bfe54c
thagreatjoel <joeljonah06@gmail.com>
2026-07-18 11:10:34 +0530

    Push 6

--- next.config.js
+++ next.config.js
@@ -1,21 +1,6 @@
-// next.config.js
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'avatars.githubusercontent.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'github.com',
-      },
-    ],
-  },
-  // ✅ Remove eslint config from here
-  // ✅ Remove typescript config from here if not needed
-}
+ 
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
